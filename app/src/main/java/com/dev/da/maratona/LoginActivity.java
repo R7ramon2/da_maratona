bdac83330f2b9fa80bcbb22779706089ba19c2d9
Tiagoemerenciano <emerenciano.tiago@gmail.com>
2017-10-10 02:13:34 -0300

    Implementações: máscara de texto no EditText de Matrícula e Senha (LoginActivity). Alterações: melhoria no layout da LoginActivity, melhorias de interação com usuário, remoção de códigos excessivos, filtro melhorado p/ saber se o usuário digitou ou não a matrícula/senha e remoção total dos warnings.

--- app/src/main/java/com/dev/da/maratona/LoginActivity.java
+++ app/src/main/java/com/dev/da/maratona/LoginActivity.java
@@ -3,14 +3,12 @@ package com.dev.da.maratona;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-    Alerta alerta;
     EditText login_input,senha_input;
     Button entrar;
     @Override
@@ -20,40 +18,10 @@ public class LoginActivity extends AppCompatActivity {
 
         login_input = (EditText) findViewById(R.id.login_input);
         senha_input = (EditText) findViewById(R.id.senha_input);
-        entrar = (Button) findViewById(R.id.logar);
+        entrar = (Button) findViewById(R.id.btnLogar);
 
-        login_input.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String textoInputOri = login_input.getText().toString();
-                if(textoInputOri.equals("Usuário")){
-                    login_input.setText("");
-                }
-            }
-        });
-
-        senha_input.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                String textoInputOri = senha_input.getText().toString();
-                if(textoInputOri.equals("Senha")){
-                    senha_input.setText("");
-                }
-                return false;
-            }
-        });
-
-        senha_input.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View view, boolean focus) {
-                String textoInputOri = senha_input.getText().toString();
-                if(focus){
-                    if(textoInputOri.equals("Senha")){
-                        senha_input.setText("");
-                    }
-                }
-            }
-        });
+        login_input.addTextChangedListener(EditTextMask.mask(login_input, EditTextMask.MATRICULA));
+        senha_input.addTextChangedListener(EditTextMask.mask(senha_input, EditTextMask.SENHA));
 
         entrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,12 +29,14 @@ public class LoginActivity extends AppCompatActivity {
                 String login = login_input.getText().toString();
                 String senha = senha_input.getText().toString();
 
-                if(login.equals("") || senha.equals("") || login.equals("Usuário") || senha.equals("Senha")){
-                    Toast.makeText(LoginActivity.this,"Login ou senha não informados",Toast.LENGTH_SHORT).show();
+                if(login.equals("")){
+                    Toast.makeText(LoginActivity.this,"Digite o login",Toast.LENGTH_SHORT).show();
+                }
+                else if(senha.equals("")){
+                    Toast.makeText(LoginActivity.this, "Digite a senha.", Toast.LENGTH_SHORT).show();
                 }
                 else{
-                    Alerta alerta = new Alerta("Olá,"+login,"Avalie o nosso app",LoginActivity.this);
-                    alerta.exibir();
+                    Toast.makeText(LoginActivity.this, "Login efetuado!", Toast.LENGTH_SHORT).show();
                     //TODO implementar acesso ao usuário (Firebase)
                 }
             }
