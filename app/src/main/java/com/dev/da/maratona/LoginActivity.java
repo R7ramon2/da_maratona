c54329a2aa55ae5b8fc98025a2c7d9e7c88d9bd7
R7ramon2 <ramonranieri@hotmail.com>
2017-10-11 14:25:12 -0300

    Ramon Ranieri
    Incluida a Activiy de propaganda e organização do código na Activity Login

--- app/src/main/java/com/dev/da/maratona/LoginActivity.java
+++ app/src/main/java/com/dev/da/maratona/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -30,17 +31,47 @@ public class LoginActivity extends AppCompatActivity {
         senha_input = (EditText) findViewById(R.id.senha_input);
         entrar = (Button) findViewById(R.id.btnLogar);
 
+        login_input.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                senha_input.setText("");
+                return false;
+            }
+        });
+
         login_input.addTextChangedListener(EditTextMask.mask(login_input, EditTextMask.MATRICULA));
         senha_input.addTextChangedListener(EditTextMask.mask(senha_input, EditTextMask.SENHA));
 
         entrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String login = login_input.getText().toString();
-                String senha = senha_input.getText().toString();
+                final String login = login_input.getText().toString();
+                final String senha = senha_input.getText().toString();
 
-                // Logar usuário, caso as informações estejam corretas
-                logar(login, senha);
+                DatabaseReference ref = firebase.child("Alunos").child(login).getRef();
+                ref.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            String nome = dataSnapshot.child("nome").getValue().toString();
+                            String senha_database = dataSnapshot.child("senha").getValue().toString();
+                            if (senha_database.equals(senha)) {
+                                logar(login, senha);
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Usuário ou senha incorretos", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else {
+                            Toast.makeText(LoginActivity.this, "Usuário ou senha incorretos", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
             }
         });
     }
@@ -56,30 +87,8 @@ public class LoginActivity extends AppCompatActivity {
         } else if (senha.length() < 6) {
             Toast.makeText(LoginActivity.this, "Senha incompleta.", Toast.LENGTH_SHORT).show();
         } else {
-            DatabaseReference ref = firebase.child("Alunos").child(matricula).getRef();
-            ref.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    if (dataSnapshot.exists()) {
-                        String nome = dataSnapshot.child("nome").getValue().toString();
-                        String senha_database = dataSnapshot.child("senha").getValue().toString();
-                        if (senha_database.equals(senha)) {
-                            Alerta alerta = new Alerta("Olá, " + nome, "Avalie nosso app", LoginActivity.this);
-                            alerta.exibir();
-                        } else {
-                            Toast.makeText(LoginActivity.this, "Usuário ou senha incorretos", Toast.LENGTH_SHORT).show();
-                        }
-                    } else {
-                        Toast.makeText(LoginActivity.this, "Usuário ou senha incorretos", Toast.LENGTH_SHORT).show();
-                    }
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-
-                }
-            });
-
+            Toast.makeText(LoginActivity.this, "Usuário logado", Toast.LENGTH_SHORT).show();
+            //TODO direcionar para o menu
         }
     }
 
