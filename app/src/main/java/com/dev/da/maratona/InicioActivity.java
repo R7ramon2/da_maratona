e9d8ee2afe2e11b07ebb396ef44c45ef99628700
Ramon Ranieri <ramonranieri@hotmail.com>
2017-10-15 13:25:40 -0200

    Ramon Ranieri
    Correção de bugs

---
+++ app/src/main/java/com/dev/da/maratona/InicioActivity.java
@@ -0,0 +1,24 @@
+package com.dev.da.maratona;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class InicioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+
+        new android.os.Handler().postDelayed(new Runnable() {
+                    public void run() {
+                        Intent it = new Intent(InicioActivity.this, LoginActivity.class);
+                        startActivity(it);
+                        finish();
+                    }
+                }, 2000);
+    }
+}
