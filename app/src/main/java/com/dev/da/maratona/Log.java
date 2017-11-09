f12803f3994a6f517d5c68006139f7e07c4f6c69
R7ramon2 <ramonranieri@hotmail.com>
2017-11-09 08:52:21 -0300

    Correção no log

--- app/src/main/java/com/dev/da/maratona/Log.java
+++ app/src/main/java/com/dev/da/maratona/Log.java
@@ -118,4 +118,6 @@ public class Log {
     public void imagem(){
         firebase.child("log").child("AdicionarImagens").child(path).child(getDateTime()).setValue("Adicionou uma uma nova imagem ao seu perfil");
     }
+
+    //teste
 }
